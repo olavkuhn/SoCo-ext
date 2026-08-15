0ef68bfb4665b9de283630092646fad04d6c5a91
Callisto Casale <callisto.casale@gmail.com>
2026-08-15 15:05:06 +0200

    init; TTS file generation

---
+++ main.py
@@ -0,0 +1,1 @@
+import SoCo_ext
---
+++ utils/tts.py
@@ -0,0 +1,44 @@
+import random
+import string
+import subprocess
+from pathlib import Path
+
+BASE_DIR = Path(__file__).parent
+TTS_DIR = BASE_DIR / "mp3s"
+
+
+class TTSGenerator:
+    def __init__(
+        self,
+        voice: str = "en-GB-RyanNeural",
+    ):
+
+        def _generate_file_identifier(self, lenght: int = 8):
+            """Generates a random alphanumeric string ID"""
+            chars = string.ascii_lowercase + string.digits
+            return "".join(random.choices(chars, k=lenght))
+
+        @property
+        def generate_file(self, text: str, filename: str):
+            """
+            This generates a single TTS audio mp3 file
+            Returns the filename on success, None on failure
+            """
+
+            _full_path = TTS_DIR / filename
+            command = [
+                "edge-tts",
+                "--text",
+                text,
+                "--write-media",
+                str(_full_path),
+                "--voice",
+                self.voice
+            ]
+
+            try:
+                subprocess.run(command, check=True, capture_output=True, text=True)
+                return filename
+            except subprocess.CalledProcessError as e:
+                print(f"Error generating {filename}: {e.stderr}")
+                return None
